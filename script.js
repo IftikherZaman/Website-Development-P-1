b69a9bf5fe8a3ac2cad7583c80badbb6b1013b5a
IftikherZaman <izcinan@gmail.com>
2024-07-20 05:21:06 -0400

    Added slidescreen

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu() {
     menu.classList.toggle("open") ;
     icon.classList.toggle("open") ;
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const slideshowContainer = document.querySelector('.slideshow-container');
+    const images = slideshowContainer.querySelectorAll('.about-pic');
+    let currentIndex = 0;
+    let intervalId;
+  
+    function showNextImage() {
+      images[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].classList.add('active');
+    }
+  
+    slideshowContainer.addEventListener('mouseenter', function() {
+      intervalId = setInterval(showNextImage, 1000); // Change image every 1 second
+    });
+  
+    slideshowContainer.addEventListener('mouseleave', function() {
+      clearInterval(intervalId);
+      // Reset to the first image when mouse leaves
+      images[currentIndex].classList.remove('active');
+      currentIndex = 0;
+      images[currentIndex].classList.add('active');
+    });
+  });
